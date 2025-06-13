b3e089385bc6cc307e195db89c4d1c4db1ee00f1
rofi222 <m.rofi0604@gmail.com>
2025-06-13 19:30:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.image("16c6dd52-2373-4b0f-8f68-667ad298874c.png", width=200)
 st.write("\n")
 st.subheader("Muhammad 'Mister' Rofi")
 st.write(
-    "Mari latihan membuat website melalui Github dan Streamlit bersama Mister Ahsan"
+    "Mari latihan membuat website melalui Github dan Streamlit bersama Mister Rofi"
 )
